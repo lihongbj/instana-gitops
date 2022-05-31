e3c08818d5e5ae41eee5768165d1f056e733d5be
lihongbj <lihongbj@cn.ibm.com>
2022-05-31 10:57:41 +0800

    update for k8s, tiny change

--- config/instana-operator/scripts/auto-instana.sh
+++ config/instana-operator/scripts/auto-instana.sh
@@ -8,13 +8,13 @@
 
 
 # install instana using sh, helm or gitops application yaml
-# 1. install-instana-ocp.sh
+# option 1. install-instana-ocp.sh
 install-instana-ocp.sh
 
-# 2. helm 
+# option 2. helm 
 helm install instana-operator --set image.tag=215-4  --namespace=instana-operator --set INSTANA_DOWNLOAD_KEY=$INSTANA_DOWNLOAD_KEY --set INSTANA_SALES_KEY=$INSTANA_SALES_KEY --set INSTANA_AGENT_KEY=$INSTANA_AGENT_KEY      instana-operator/
 
-# 3. gitops argocd app
+# option 3. gitops argocd app
 kubeapi=$(kubectl config current-context | cut -f2 -d/ | sed "s/-/./g")
 kube_master="https://"$kubeapi
 
